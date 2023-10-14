d789abab14ca6b2448e5bffbd9fbb96235b547ff
Guillaume Havard <guill.havard@gmail.com>
2023-10-14 17:54:02 +0200

    chore: Add python checkers

--- src/test_flask/app.py
+++ src/test_flask/app.py
@@ -3,18 +3,26 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
+    """Main page."""
     return "<p>Main page</p>"
+
 
 @app.route("/hello")
 def hello_world():
+    """Hello to everyone"""
     return "<p>Coucou</p>"
+
 
 @app.route("/hello/<string:name>")
 def hello_name(name: str):
+    """Hellot to someone."""
     return f"<p>Coucou {escape(name)}</p>"
 
-@app.route("/id/<int:id>")
-def hello_id(id: int):
-    return f"<p>Coucou n°:{id}</p>"    
+
+@app.route("/id/<int:uid>")
+def hello_id(uid: int):
+    """Hello to an id."""
+    return f"<p>Coucou n°:{uid}</p>"
