6c6e04b9071b3ab513cdd1d8f45a45c3c9d6a671
Guillaume Havard <guill.havard@gmail.com>
2023-10-15 20:14:29 +0200

    feat: Add user page
    Users are randomly generated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Test of Flask framework
+
+## RUn in debug mode
+
+    poetry run flask run --debug
--- src/test_flask/app.py
+++ src/test_flask/app.py
@@ -1,3 +1,5 @@
+from faker import Faker
+from faker.providers import date_time, person, python
 from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
@@ -13,3 +15,24 @@ def main():
 def index():
     """Main page."""
     return redirect(url_for("main"))
+
+
+@app.route("/users")
+def users_administration():
+    """Users administration page."""
+    fake = Faker()
+    fake.add_provider(person)
+    fake.add_provider(python)
+    fake.add_provider(date_time)
+    users = []
+    for _ in range(10):
+        users.append(
+            {
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name(),
+                "age": fake.pyint(18, 80),
+                "member_since": fake.date("%d/%m/%Y"),
+            }
+        )
+
+    return render_template("users.html", users=users)
